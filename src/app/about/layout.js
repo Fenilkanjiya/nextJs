94a995d6d7698fe4947a5c7cf22bfc09c0c6e93a
eg-fenilkanjiya <fenilkanjiya1611@gmail.com>
2025-02-04 08:01:53 +0530

    conditional routing

--- src/app/about/layout.js
+++ src/app/about/layout.js
@@ -1,20 +1,27 @@
+'use client';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 export default function Layout({ children }) {
+  const pathName = usePathname();
   return (
     <>
       <h1>Layout page</h1>
-      <ul>
-        <li>
-          <Link href="/about">About</Link>
-        </li>
-        <li>
-          <Link href="/about/aboutTeacher">Teacher</Link>
-        </li>
-        <li>
-          <Link href="/about/aboutStudent">Student</Link>
-        </li>
-      </ul>
+      {pathName !== '/about/aboutStudent' ? (
+        <ul>
+          <li>
+            <Link href="/about">About</Link>
+          </li>
+          <li>
+            <Link href="/about/aboutTeacher">Teacher</Link>
+          </li>
+          <li>
+            <Link href="/about/aboutStudent">Student</Link>
+          </li>
+        </ul>
+      ) : (
+        <Link href="/">go to Home</Link>
+      )}
       {children}
     </>
   );
